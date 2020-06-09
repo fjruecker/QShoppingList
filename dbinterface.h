a54069a4288e2dffdbcf9b54360729482f3ce9d8
Florian Rücker <f.j.ruecker@gmx.net>
2020-06-09 16:22:12 +0200

    FEATURE: Add declarations in dbinterface.h

--- dbinterface.h
+++ dbinterface.h
@@ -2,15 +2,25 @@
 #define DBINTERFACE_H
 
 #include <QObject>
+#include <QtSql>
 
 class DbInterface : public QObject
 {
     Q_OBJECT
 public:
     explicit DbInterface(QObject *parent = nullptr);
+    bool initDatabase(QString path);
 
 signals:
+    void getData(QList<QMap<QString,QVariant>>);
+    void addData(int id);
 
+public slots:
+    int addData(QMap<QString,QVariant> data);
+    QList<QMap<QString,QVariant>> getData();
+
+private:
+    QSqlDatabase mDb;
 };
 
 #endif // DBINTERFACE_H

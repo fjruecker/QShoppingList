6168f17ad92ae3dd568c233dc0264bd670205099
Florian Rücker <f.j.ruecker@gmx.net>
2020-06-09 16:19:07 +0200

    FEATURE: Find place for db

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QStandardPaths>
+#include <QDir>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QApplication::setApplicationName("QShoppingList");
+    QString db = QStandardPaths::locate(QStandardPaths::AppDataLocation, "database.db");
+    if(db.isEmpty()) {
+        QString path = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+        if(! QDir(path).exists()) {
+            QDir().mkpath(path);
+        }
+        db = path + QDir::separator() + "database.db";
+    }
 }
 
 MainWindow::~MainWindow()

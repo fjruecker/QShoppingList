a5807d2552e9376ac19bfbf7d28d0da0ce8637b7
Florian Rücker <f.j.ruecker@gmx.net>
2020-06-09 16:30:23 +0200

    FEATURE: Add Data to db

--- dbinterface.cpp
+++ dbinterface.cpp
@@ -24,3 +24,26 @@ bool DbInterface::initDatabase(QString path)
 
     return true;
 }
+
+int DbInterface::addData(QMap<QString, QVariant> data)
+{
+    QSqlQuery query;
+    query.prepare("INSERT INTO Shopping (Status, Item) VALUES (?, ?)");
+    query.bindValue(0, data.value("Status"));
+    query.bindValue(1, data.value("Item"));
+
+    if(! query.exec()) {
+        qDebug() << "Error adding item";
+        emit addData(-1);
+        return -1;
+    }
+
+    int id = query.lastInsertId().toInt();
+    emit addData(id);
+    return id;
+}
+
+QList<QMap<QString, QVariant> > DbInterface::getData()
+{
+
+}
